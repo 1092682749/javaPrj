2c8285bbecc04f5a8b5d6b796bad01a6658e8e02
PC\john <1092682749@qq.com>
2018-04-25 13:37:03 +0800

    bug

--- src/main/java/com/qy/controller/AdminController.java
+++ src/main/java/com/qy/controller/AdminController.java
@@ -6,15 +6,20 @@ import com.qy.service.AdminService;
 import com.qy.base.core.PageBean;
 import com.github.pagehelper.PageHelper;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.annotation.Resource;
+import javax.jws.WebParam;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
 * Created by zaq on 2018/04/14.
 */
 @RestController
 @RequestMapping("/admin")
+@SessionAttributes(names = {"admin"})
 public class AdminController {
     @Resource
     private AdminService adminService;
@@ -49,5 +54,22 @@ public class AdminController {
         List<Admin> list = adminService.findAll();
         page.setList(list);
         return ResultGenerator.successResult(page);
+    }
+    @RequestMapping("/login")
+    public ModelAndView login(){
+        ModelAndView mav = new ModelAndView("admin/login");
+        return mav;
+    }
+    @RequestMapping("/verify")
+    @ResponseBody
+    public Map<String,Object> verify(Admin admin){
+        Map<String,Object> map = new HashMap<>();
+        map.put("bool",false);
+        map.put("msg","登陆失败!");
+        return map;
+    }
+    @RequestMapping("index")
+    public ModelAndView index(){
+        return new ModelAndView("admin/index");
     }
 }
